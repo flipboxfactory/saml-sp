2e904e68c380079bdb7b70a42a3c43ff555beb04
Damien Smrt <damien@flipboxdigital.com>
2018-03-26 23:42:22 -0600

    fixing cli

--- src/cli/KeyChain.php
+++ src/cli/KeyChain.php
@@ -9,6 +9,7 @@
 namespace flipbox\saml\sp\cli;
 
 
+use flipbox\keychain\keypair\traits\OpenSSLCliUtil;
 use flipbox\saml\sp\Saml;
 use yii\console\Controller;
 use flipbox\keychain\keypair\traits\OpenSSL as OpenSSLTrait;
@@ -17,7 +18,7 @@ use flipbox\keychain\keypair\traits\OpenSSLCli as OpenSSLCliTrait;
 class KeyChain extends Controller
 {
 
-    use OpenSSLTrait, OpenSSLCliTrait;
+    use OpenSSLTrait, OpenSSLCliTrait, OpenSSLCliUtil;
     /**
      * @var bool $force
      * Force save the metadata. If one already exists, it'll be overwritten.
