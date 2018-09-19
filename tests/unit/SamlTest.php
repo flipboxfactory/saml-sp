5ae9d8e116f0c18dac0d795e3cfb4b748dcbcfa6
Doug St. John <3673451+dougstjohn@users.noreply.github.com>
2018-09-18 18:24:01 -0600

    Test component configuration

--- tests/unit/SamlTest.php
+++ tests/unit/SamlTest.php
@@ -5,6 +5,17 @@ namespace flipbox\saml\sp\tests;
 use Codeception\Test\Unit;
 use flipbox\saml\sp\Saml;
 use flipbox\saml\sp\services\Login;
+use flipbox\saml\sp\services\Cp;
+use flipbox\saml\sp\services\Provider;
+use flipbox\saml\sp\services\ProviderIdentity;
+use flipbox\saml\sp\services\bindings\HttpPost;
+use flipbox\saml\sp\services\bindings\HttpRedirect;
+use flipbox\saml\sp\services\messages\AuthnRequest;
+use flipbox\saml\sp\services\messages\LogoutRequest;
+use flipbox\saml\sp\services\messages\LogoutResponse;
+use flipbox\saml\sp\services\messages\Metadata;
+use flipbox\saml\sp\services\messages\Response;
+use flipbox\saml\core\services\Session;
 
 class SamlTest extends Unit
 {
@@ -19,17 +30,22 @@ class SamlTest extends Unit
      */
     protected function _before()
     {
-        $this->module = new Saml('saml-sp');
+        $this->module = Saml::getInstance();
     }
 
-    /**
-     * Test the component is set correctly
-     */
-    public function testLoginComponent()
+    public function testComponentConfiguration()
     {
-        $this->assertInstanceOf(
-            Login::class,
-            $this->module->getLogin()
-        );
+        $this->assertInstanceOf(AuthnRequest::class, $this->module->getAuthnRequest());
+        $this->assertInstanceOf(HttpPost::class, $this->module->getHttpPost());
+        $this->assertInstanceOf(HttpRedirect::class, $this->module->getHttpRedirect());
+        $this->assertInstanceOf(Login::class, $this->module->getLogin());
+        $this->assertInstanceOf(LogoutRequest::class, $this->module->getLogoutRequest());
+        $this->assertInstanceOf(LogoutResponse::class, $this->module->getLogoutResponse());
+        $this->assertInstanceOf(Provider::class, $this->module->getProvider());
+        $this->assertInstanceOf(ProviderIdentity::class, $this->module->getProviderIdentity());
+        $this->assertInstanceOf(Metadata::class, $this->module->getMetadata());
+        $this->assertInstanceOf(Response::class, $this->module->getResponse());
+        $this->assertInstanceOf(Session::class, $this->module->getSession());
+        $this->assertInstanceOf(Cp::class, $this->module->getCp());
     }
 }
