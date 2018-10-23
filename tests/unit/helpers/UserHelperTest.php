a793da633cde710ed3272d640f5cfbe119f0abbe
Doug St. John <3673451+dougstjohn@users.noreply.github.com>
2018-10-23 10:07:30 -0600

    UserHelper test WIP

--- tests/unit/helpers/UserHelperTest.php
+++ tests/unit/helpers/UserHelperTest.php
@@ -9,49 +9,140 @@ use flipbox\saml\sp\helpers\UserHelper;
 
 class UserHelperTest extends Unit
 {
+    private $suspendedUser;
+    private $lockedUser;
+    private $disabledUser;
+    private $archivedUser;
+    private $activeUser;
+
+    /**
+     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
+     * phpcs:disable PSR2.Methods.MethodDeclaration.Underscore
+     */
+/*    protected function _before()
+    {
+        $this->suspendedUser = $this->createSuspendedUser();
+        $this->lockedUser = $this->createLockedUser();
+        $this->disabledUser = $this->createDisabledUser();
+        $this->archivedUser = $this->createArchivedUser();
+        $this->activeUser = $this->createActiveUser();
+    }
+
     public function testEnablingSuspendedUser()
     {
+        UserHelper::enableUser($this->suspendedUser);
 
-        $user = new User([
-            'username' => 'test+new-user1@test.com',
-            'email' => 'test+new-user1@test.com',
-            'suspended' => true,
-        ]);
-
-        Craft::$app->elements->saveElement($user);
-        UserHelper::enableUser($user);
-
-        $this->assertFalse($user->suspended);
+        $this->assertEquals(0, $this->suspendedUser->suspended);
+        $this->assertEquals(0, $this->suspendedUser->enabled);
     }
 
-    // User not enabled
+    public function testEnablingLockedUser()
+    {
+        UserHelper::enableUser($this->lockedUser);
+
+        $this->assertEquals(0, $this->lockedUser->locked);
+        $this->assertEquals(1, $this->lockedUser->enabled);
+    }
+
     public function testEnablingDisabledUser()
     {
-        \Craft::$app->elements->saveElement(
-            $user = new User([
-                'username' => 'test+new-user3@test.com',
-                'email' => 'test+new-user3@test.com',
-                'enabled' => 0,
-            ])
-        );
+        UserHelper::enableUser($this->disabledUser);
 
-        UserHelper::enableUser($user);
-
-        $this->assertTrue($user->enabled);
+        $this->assertEquals(1, $this->disabledUser->enabled);
     }
 
-    public function testEnablingArchived()
+    public function testEnablingArchivedUser()
     {
+        UserHelper::enableUser($this->archivedUser);
+
+        $this->assertEquals(1, $this->archivedUser->archived);
+        $this->assertEquals(1, $this->archivedUser->enabled);
+    }
+
+    public function testEnablingActiveUser()
+    {
+        UserHelper::enableUser($this->activeUser);
+
+        $this->assertEquals(1, $this->activeUser->active);
+        $this->assertEquals(1, $this->activeUser->enabled);
+    }
+
+    public function testIsUserSuspended()
+    {
+        $this->assertEquals(1, $this->suspendedUser->suspended);
+        $this->assertEquals(0, $this->lockedUser->suspended);
+    }
+
+    public function testIsUserLocked()
+    {
+        $this->assertEquals(1, $this->lockedUser->locked);
+        $this->assertEquals(0, $this->activeUser->locked);
+    }
+
+    public function testIsUserPending()
+    {
+        $this->assertEquals(1, $this->disabledUser->pending);
+        $this->assertEquals(0, $this->lockedUser->pending);
+    }
+
+    public function testIsUserArchived()
+    {
+        $this->assertEquals(1, $this->archivedUser->archived);
+        $this->assertEquals(0, $this->lockedUser->archived);
+    }
+
+    public function testIsUserActive()
+    {
+        $this->assertEquals(1, $this->activeUser->active);
+        $this->assertEquals(0, $this->lockedUser->active);
+    }
+
+
+    private function fakeEmail()
+    {
+        return 'test+' . random_int(1, 10000000) . '@test.com';
+    }
+
+    private function createSuspendedUser()
+    {
+        $user = $this->createUser();
+        Craft::$app->getUsers()->suspendUser($user);
+
+        return $user;
+    }
+
+    private function createLockedUser()
+    {
+        return $this->createUser('locked', 1);
+    }
+
+    private function createDisabledUser()
+    {
+        return $this->createUser('pending', 1);
+    }
+
+    private function createArchivedUser()
+    {
+        return $this->createUser('archived', 1);
+    }
+
+    private function createActiveUser()
+    {
+        return $this->createUser('active', 1);
+    }
+
+    private function createUser($status, $value)
+    {
+        $fakeEmail = $this->fakeEmail();
+
         \Craft::$app->elements->saveElement(
             $user = new User([
-                'username' => 'test+new-user4@test.com',
-                'email' => 'test+new-user4@test.com',
-                'archived' => true,
+                'username' => $fakeEmail,
+                'email' => $fakeEmail,
+                $status => $value
             ])
         );
 
-        UserHelper::enableUser($user);
-
-        $this->assertFalse($user->archived);
+        return $user;
     }
-}
+}*/
