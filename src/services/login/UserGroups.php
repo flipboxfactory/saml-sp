b0b1a11a10eaa1d205c6668b7fdf9b37f3089ac1
Damien Smrt <damien@flipboxdigital.com>
2018-10-24 14:35:10 -0600

    Fixing bug with mapping groups from response removing existing groups. https://github.com/flipboxfactory/saml-sp/issues/19

--- src/services/login/UserGroups.php
+++ src/services/login/UserGroups.php
@@ -123,7 +123,25 @@ class UserGroups
             return true;
         }
 
-        return \Craft::$app->getUsers()->assignUserToGroups($user->id, $groups);
+        /**
+         * Get existing groups
+         */
+        $existingGroupIds = array_map(
+            function ($group) {
+                return (int)$group->id;
+            },
+            $user->getGroups()
+        );
+
+        return \Craft::$app->getUsers()->assignUserToGroups(
+            $user->id,
+            array_unique(
+                array_merge(
+                    $existingGroupIds,
+                    $groups
+                )
+            )
+        );
     }
 
     /**
