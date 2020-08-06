b21ccb49dedea62c3c0403a9e59f77a41f712967
Damien Smrt <damien@flipboxdigital.com>
2020-08-06 09:47:50 -0600

    Issue with constraint on the Provider Identity table when the users NameID changes.

---
+++ src/migrations/m200806_200000_provider_identity_constraint.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace flipbox\saml\sp\migrations;
+
+use flipbox\saml\core\migrations\m200806_200000_provider_identity_constraint as abstractMigration;
+use flipbox\saml\sp\records\ProviderIdentityRecord;
+
+/**
+ */
+class m200806_200000_provider_identity_constraint extends abstractMigration
+{
+
+    protected static function getIdentityTableName()
+    {
+        return ProviderIdentityRecord::tableName();
+    }
+}
